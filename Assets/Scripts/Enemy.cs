1974fe0874fceee580ddc6a38d74afe67a600634
coding-cuber <73984595+coding-cuber@users.noreply.github.com>
2021-11-05 18:09:21 +1100

    enemies stop when near player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,14 +1,36 @@
+using System;
 using Pathfinding;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Enemy : MonoBehaviour
 {
     public float health;
     public GameManager gm;
 
-    private void Update()
+    private Collider2D targetCollider;
+
+    private void Start()
     {
-        gameObject.GetComponent<AIPath>().enabled = !(Vector2.Distance(transform.position, GetComponent<AIDestinationSetter>().target.position) <= 1.4f);
+        targetCollider = GetComponent<AIDestinationSetter>().target.GetComponent<Collider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<AIPath>().maxSpeed = IsTargetInRange() ? 0 : 2;
+    }
+    
+    private bool IsTargetInRange()
+    {
+        Collider2D[] hit = Physics2D.OverlapCircleAll(transform.position, 4.5f);
+
+        if (hit.Contains(targetCollider))
+        {
+            return true;
+        }
+
+        return false;
     }
 
     public void TakeDamage(float amount)
