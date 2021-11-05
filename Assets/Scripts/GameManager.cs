6a11cbc3f73a44ff9804f46cb769648c77964a47
coding-cuber <73984595+coding-cuber@users.noreply.github.com>
2021-11-05 16:00:11 +1100

    enemy spawning at colony, fixed bug where an entire army would be respawned when one troop died

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,55 +6,53 @@ using Random = UnityEngine.Random;
 public class GameManager : MonoBehaviour
 {
     public Transform playerTransform;
-    
+    public float spawnRadius = 5f;
+    public int enemies;
+
+    [SerializeField] private Transform enemySpawnLocation;
     [SerializeField] private List<GameObject> commonTroops; //This array stores the common troops (60% spawn rate)
     [SerializeField] private List<GameObject> rareTroops; //This array stores the rare troops (30% spawn rate)
     [SerializeField] private List<GameObject> arialTroops; //This array stores the arial troops such as wasps (10% spawn rate)
     [SerializeField] private int armySize;
-    public int enemies;
 
     private void Update()
     {
         //Spawning enemy troops
         if (enemies < armySize)
         {
+            Debug.Log(enemies);
             int enemiesRequired = armySize - enemies;
 
-            for (int i = 0; i < armySize; i++)
+            for (int i = 0; i < enemiesRequired; i++)
             {
                 int c = Random.Range(0, 11);
+                Vector3 spawnLocation = enemySpawnLocation.position + Random.onUnitSphere * spawnRadius;
 
                 if (c <= 6)
                 {
                     //Spawn common troop
-                    GameObject troop = Instantiate(commonTroops[Random.Range(0, commonTroops.Count)], transform.position, Quaternion.identity); //TODO: Spawn at colony position
+                    GameObject troop = Instantiate(commonTroops[Random.Range(0, commonTroops.Count)], spawnLocation, Quaternion.identity);
                     troop.GetComponent<AIDestinationSetter>().target = playerTransform;
                     troop.GetComponent<Enemy>().gm = this;
                     enemies += 1;
-                    
-                    Debug.Log("Spawned common enemy troop");
                 } 
                 else if (c > 6)
                 {
                     if (c < 90)
                     {
                         //Spawn arial troop
-                        GameObject troop = Instantiate(arialTroops[Random.Range(0, commonTroops.Count)], transform.position, Quaternion.identity);
+                        GameObject troop = Instantiate(arialTroops[Random.Range(0, commonTroops.Count)], spawnLocation, Quaternion.identity);
                         troop.GetComponent<AIDestinationSetter>().target = playerTransform;
                         troop.GetComponent<Enemy>().gm = this;
                         enemies += 1;
-                        
-                        Debug.Log("Spawned arial enemy troop");
                     }
                     else
                     {
                         //Spawn rare troop
-                        GameObject troop = Instantiate(rareTroops[Random.Range(0, commonTroops.Count)], transform.position, Quaternion.identity);
+                        GameObject troop = Instantiate(rareTroops[Random.Range(0, commonTroops.Count)], spawnLocation, Quaternion.identity);
                         troop.GetComponent<AIDestinationSetter>().target = playerTransform;
                         troop.GetComponent<Enemy>().gm = this;
                         enemies += 1;
-                        
-                        Debug.Log("Spawned rare enemy troop");
                     }
                 }
             }
