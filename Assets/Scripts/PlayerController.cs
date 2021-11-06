1611a5fa2b2f66f389bae606053d3ae48e7e1e64
William Herring <73984595+coding-cuber@users.noreply.github.com>
2021-11-06 15:30:40 +1100

    walking animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,23 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody2D rb; //Stores attached RigidBody2D
     [SerializeField] private float moveSpeed, turnSpeed; //Stores player speeds
 
+    [SerializeField] private Animator anim;
+
     private void FixedUpdate()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector2 direction = new Vector2(horizontal, vertical) * (moveSpeed * Time.deltaTime);
+
+        if (direction != Vector2.zero)
+        {
+            anim.SetBool("Walking", true);
+        }
+        else
+        {
+            anim.SetBool("Walking", false);
+        }
 
         rb.velocity = direction;
     }
