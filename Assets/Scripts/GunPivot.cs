02f47e2307f865892a2217a41121694ecdfd9541
William Herring <73984595+coding-cuber@users.noreply.github.com>
2021-11-06 12:07:50 +1100

    gun pivot

---
+++ Assets/Scripts/GunPivot.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunPivot : MonoBehaviour
+{
+    private void Update()
+    {
+        PointToMouse();
+    }
+    
+    private void PointToMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 5.23f;
+
+        Vector3 objPosition = Camera.main.WorldToScreenPoint(transform.position);
+        mousePosition.x = mousePosition.x - objPosition.x;
+        mousePosition.y = mousePosition.y - objPosition.y;
+ 
+        float angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * 0.8f * Mathf.Rad2Deg; //Don't ask me how this actually works
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,6 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody2D rb; //Stores attached RigidBody2D
     [SerializeField] private float moveSpeed, turnSpeed; //Stores player speeds
 
-    private void Update()
-    {
-        PointToMouse();
-    }
-    
     private void FixedUpdate()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -19,18 +14,5 @@ public class PlayerController : MonoBehaviour
         Vector2 direction = new Vector2(horizontal, vertical) * (moveSpeed * Time.deltaTime);
 
         rb.velocity = direction;
-    }
-    
-    private void PointToMouse()
-    {
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition.z = 5.23f;
-
-        Vector3 objPosition = Camera.main.WorldToScreenPoint(transform.position);
-        mousePosition.x = mousePosition.x - objPosition.x;
-        mousePosition.y = mousePosition.y - objPosition.y;
- 
-        float angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * turnSpeed * Mathf.Rad2Deg; //Don't ask me how this actually works
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
     }
 }
