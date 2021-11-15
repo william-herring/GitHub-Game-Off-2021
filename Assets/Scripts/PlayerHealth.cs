00562ee8b192a4d6790522263833d65a0e21b2e4
williamherring <william.herring.au@gmail.com>
2021-11-15 17:24:02 +1100

    ui bugs fixed

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,9 +1,19 @@
+using System;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private float health = 4f;
     public GameManager gm;
+
+    private void Update()
+    {
+        //FOR TESTING PURPOSES ONLY
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(200f);
+        }
+    }
 
     public void TakeDamage(float damage)
     {
